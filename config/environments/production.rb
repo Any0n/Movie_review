9b7e0da025ddf7c7595d9fa6a5b28db4421885ae
Animesh Maharjan <animesh_maharjan@hotmail.com>
2016-04-04 09:13:55 +0545

    more fixes for image path setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # devise for prodution 
-  #config.action_mailer.default_url_options = { :host => '' }
+  config.action_mailer.default_url_options = { :host => 'https://maimoviereview.herokuapp.com' }
 end
